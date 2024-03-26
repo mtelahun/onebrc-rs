8ac6385ffd6e7818160ec2acf9b055800178dceb
Michael Telahun <michael.telahun@dindin.et>
2024-03-26 13:20:54 +0300

    test: input file does not exist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    #[test]
+    fn given_nonexistent_file_return_error() {
+        // Act
+        let measurements = StationMeasurements::from_file("./nonexistent");
+
+        // Assert
+        assert!(measurements.is_err());
+        assert_eq!(
+            Err(io::ErrorKind::NotFound),
+            measurements,
+            "failed to open measurements file"
+        );
+    }
+}
